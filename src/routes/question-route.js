1d696c30857dea52ae0c817a0543b74a71ed39e5
pallabcodes <kayydraws@gmail.com>
2024-07-23 12:45:50 +0530

    chore: clean up and fixed some typos

--- src/routes/question-route.js
+++ src/routes/question-route.js
@@ -4,7 +4,20 @@ const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const { jwtAuthGuard } = require('../middlewares/jwtAuthGuard');
 
+/**
+ * @desc   Get questions by category or categories
+ * @route  GET /api/questions
+ * @access Private
+ */
+
 router.get('/', jwtAuthGuard, QuestionController.getQuestionsForEachCategory);
+
+
+/**
+ * @desc   Create questions in bulk from a csv file
+ * @route  POST /api/questions/bulk-import
+ * @access Private
+ */
 
 router.post(
   '/bulk-import',
